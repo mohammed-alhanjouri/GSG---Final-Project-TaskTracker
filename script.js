3b00df362e393cf7ac5103599ded6e2b84cf36b4
Mohammed Al Hanjouri <mohammadalhanjouri@gmail.com>
2024-10-24 18:51:03 +0300

    new commits

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const tasks =[];
 
 const addTask = () => {
+    const taskName = document.getElementById('taskName');
+    const isDuplicate = tasks.some(task => task.taskName.toLowerCase() === taskName.toLowerCase());
+    if (taskName.trim() === "") {
+        alert("You have to write Task Title!");
+    }
+    else if (isDuplicate) {
+        alert("This Task Title already exists!");
+    }
+    else {
+        const newTask = {taskName , isCompleted: false};
+        tasks.push(newTask);
+    }
 };
